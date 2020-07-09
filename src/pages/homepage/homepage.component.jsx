4f9f6f88b2ec86905a4f266d8990d0d7eae47aaa
RohanLodhi <rogth7@gmail.com>
2020-07-09 22:17:23 +0530

    "pull"

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,18 +3,10 @@ import './homepage.styles.css'
 import ChallengeView from './../../components/challenge-view/challenge-view.component'
 import Details from './../../components/details/details.component'
 
-<<<<<<< HEAD
-const HomePage = () => {
-  return (
-    <div>
-      <Details />
-      <ChallengeView />
-=======
 const HomePage = () => (
     <div> 
         <Details />
         <ChallengeView />
->>>>>>> 7f5edf808f70adcbed2882cf7242804e08e0c8a4
     </div>
 )
 
