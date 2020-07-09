016d6d457858021f00678e94392ace31d3d14fb2
RohanLodhi <rogth7@gmail.com>
2020-07-09 19:10:25 +0530

    Added 404 and routing fix

--- src/App.js
+++ src/App.js
@@ -7,24 +7,37 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import ChallengePage from './pages/challenge-page/challenge-page.component';
 
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path='/'>
-          <HomePage />
-        </Route>
-        <Route path='/:id'>
-            <ChallengePage />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    const qs = window.location.search;
+    let c = new URLSearchParams(qs);
+    if (c.get('__loader')) {
+      const route = c.get('__loader');
+      let history = useHistory();
+      history.push(route);
+      return;
+    }
+  }
+  render() {
+    return(
+      <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route path='/:id'>
+              <ChallengePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    )
+  };
 }
 
 export default App;
