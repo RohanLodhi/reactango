3db84499c353615049a8b281789bfec26c6415ae
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2020-07-09 22:22:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,16 +11,13 @@ import { useHistory } from "react-router-dom";
 import ChallengePage from './pages/challenge-page/challenge-page.component';
 
 function App() {
-  React.useEffect(() => {
-    const qs = window.location.search;
+   const qs = window.location.search;
     let c = new URLSearchParams(qs);
     let history = useHistory();
     if (c.get("__loader")) {
       const route = c.get("__loader");
       history.push(route);
-      return;
     }
-  }, []);
   return (
     <Router>
       <div className="App">
